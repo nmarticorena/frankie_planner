fd44e5a6b9d88c2fe8193ec0f6cf6b4b7e1b76ec
nmarticorena <nicolas.marticorena.vidal@gmail.com>
2025-04-02 11:36:58 +1000

    Refactor code and add script to get metrics on a particular scene

---
+++ frankie_planner/moveit_utils.py
@@ -0,0 +1,128 @@
+import json
+
+import spatialmath as sm
+import spatialmath.base as smb
+
+# Ros imports
+from geometry_msgs.msg import Pose
+from moveit_msgs.msg import CollisionObject, DisplayRobotState
+from shape_msgs.msg import SolidPrimitive
+from moveit_configs_utils import MoveItConfigsBuilder
+
+from moveit.planning import MoveItPy
+
+
+def plan(
+    planning_component,
+    logger,
+):
+    """Helper function to plan and execute a motion."""
+    # plan to goal
+    logger.info("Planning trajectory")
+    plan_result = planning_component.plan()
+
+    if plan_result:
+        logger.info("Plan success")
+        return True
+    else:
+        logger.error("Planning failed")
+        return False
+
+
+def load_rmmi_scene(planning_scene_monitor, file_path:str)-> sm.SE3:
+    """Helper function that adds collision objects to the planning scene."""
+    with open(file_path, 'r') as file:
+        data = json.load(file)
+
+    boxes = data["cubes"]
+    cylinders = data["cylinders"]
+
+    with planning_scene_monitor.read_write() as scene:
+        collision_object = CollisionObject()
+        collision_object.header.frame_id = "odom"
+        collision_object.id = "boxes"
+
+        for position, dimensions in zip(
+                (b["position"] for b in boxes), (b["scale"] for b in boxes)
+            ):
+            box_pose = Pose()
+            box_pose.position.x = position[0]
+            box_pose.position.y = position[1]
+            box_pose.position.z = position[2]
+            dimensions = [d * 2 for d in dimensions]
+            
+
+            box = SolidPrimitive()
+            box.type = SolidPrimitive.BOX
+            box.dimensions = dimensions
+    
+            collision_object.primitives.append(box)
+            collision_object.primitive_poses.append(box_pose)
+            collision_object.operation = CollisionObject.ADD
+        for position, r , h in zip(
+                (c["position"] for c in cylinders), (c["radius"] for c in cylinders), (c["heigth"] for c in cylinders)
+            ):
+            box_pose = Pose()
+            box_pose.position.x = position[0]
+            box_pose.position.y = position[1]
+            box_pose.position.z = position[2]
+            
+
+            box = SolidPrimitive()
+            box.type = SolidPrimitive.CYLINDER
+            box.dimensions = [h * 2, r]
+    
+            collision_object.primitives.append(box)
+            collision_object.primitive_poses.append(box_pose)
+            collision_object.operation = CollisionObject.ADD
+
+        scene.apply_collision_object(collision_object)
+        scene.current_state.update()  # Important to ensure the scene is updated
+
+
+    target_pose = sm.SE3(data["target"], check = False) * sm.SE3.Tz(-0.1034) 
+    
+    return target_pose
+
+def convert_to_display_robot_state(robot_state_msg):
+    """
+    Converts a moveit::core::RobotState to moveit_msgs::DisplayRobotState.
+
+    :param robot_state: The MoveIt RobotState object
+    :param robot_model: The MoveIt RobotModel object (used for visualizing)
+    :return: Corresponding moveit_msgs.msg.DisplayRobotState message
+    """
+    # Convert to moveit_msgs::RobotState
+    # Create DisplayRobotState
+    display_robot_state_msg = DisplayRobotState()
+    display_robot_state_msg.state = robot_state_msg
+
+    return display_robot_state_msg
+
+def sm_to_ros(pose: sm.SE3):
+    pose_msg = Pose()
+    x,y,z = pose.t
+    pose_msg.position.x = x
+    pose_msg.position.y = y
+    pose_msg.position.z = z
+    q = smb.r2q(pose.R, order = "xyzs")
+    pose_msg.orientation.x = q[0]
+    pose_msg.orientation.y = q[1]
+    pose_msg.orientation.z = q[2]
+    pose_msg.orientation.w = q[3]
+
+    return pose_msg
+    
+def get_planning_interface() -> MoveItPy:
+    moveit_config = (
+        MoveItConfigsBuilder(robot_name="frankie", package_name="frankie_moveit_config")
+        .robot_description_semantic(file_path="config/frankie.srdf")
+        .robot_description(file_path="config/frankie.urdf.xacro")
+        .joint_limits(file_path="config/joint_limits.yaml")
+        .sensors_3d(file_path="config/sensors_3d.yaml")
+        .robot_description_kinematics(file_path="config/kinematics.yaml")
+        .trajectory_execution(file_path="config/copy_controllers.yaml")
+        .moveit_cpp(file_path="config/moveit_cpp.yaml")
+    ).to_moveit_configs().to_dict()
+    return MoveItPy(node_name="moveit_py", config_dict = moveit_config)
+
---
+++ scripts/load_rmmi.py
@@ -0,0 +1,131 @@
+#!/usr/bin/env python3
+"""
+Shows how to use a planning scene in MoveItPy to add collision objects and perform collision checking.
+"""
+import pandas as pd
+import copy
+import time
+import rclpy
+from rclpy.logging import get_logger
+import spatialmath.base as smb
+import time
+
+from moveit.planning import MoveItPy, PlanRequestParameters
+
+from moveit.core.robot_state import RobotState, robotStateToRobotStateMsg
+
+from geometry_msgs.msg import PoseStamped
+import moveit_msgs
+
+import frankie_planner.moveit_utils as utils
+
+# config file libraries
+
+
+def main():
+    ###################################################################
+    # MoveItPy Setup
+    ###################################################################
+    rclpy.init()
+    logger = get_logger("moveit_py_planning_scene")
+    
+    planning_interface = utils.get_planning_interface()
+
+    pose_node = rclpy.create_node("pose_publisher")
+    pose_pub = pose_node.create_publisher(PoseStamped, "debug_pose", 10)
+    robot_state_pub = pose_node.create_publisher(moveit_msgs.msg.DisplayRobotState, "ik_sol", 10)
+    robot = planning_interface.get_planning_component("mobile_base_arm")
+    robot.set_workspace(-0,-1.0,-1.,1.,2.5,3.)
+    logger.info("MoveItPy instance created")
+
+    planning_scene_monitor = planning_interface.get_planning_scene_monitor()
+
+    package_path =  "/home/nmarticorena/Documents/tutorials/moveit/jazzy_ws/src/frankie_planner/"
+    
+    working = 0 # To have a rolling avg window
+
+    episode_template = {
+        "name": "",
+        "solved" : False,
+        "IK_total": 0,
+        "IK_avg": 0,
+        "plan_tries" : 0,
+        "wall_time" : 0,
+    }     
+    exp_data = []
+
+
+    for i in range(500):
+        data = copy.deepcopy(episode_template)
+        robot.set_start_state(configuration_name="home")
+        exp_name = f"bookshelf_cage_{i:04d}"
+
+        data["name"] = exp_name
+        target_pose = utils.load_rmmi_scene(planning_scene_monitor, f"{package_path}/data/bookshelf_cage/{exp_name}.json")
+        
+        # target_pose = utils.load_rmmi_scene(planning_scene_monitor, f"{package_path}/data/table_new/table_new_{i:04d}.json")
+        robot_state = RobotState(planning_interface.get_robot_model())
+        # Set the pose goal
+        pose_goal = utils.sm_to_ros(target_pose)
+
+        pose_msg = PoseStamped()
+        pose_msg.header.frame_id = "odom"  # Ensure this matches your TF setup
+        pose_msg.header.stamp = pose_node.get_clock().now().to_msg()
+        pose_msg.pose = pose_goal
+
+        pose_pub.publish(pose_msg)
+        
+        ti = time.perf_counter()
+        solved = False
+        for plan_tries in range(100):
+            ik_solved = False
+            total_ik_tries = 0
+            for ik_tries in range(1000):
+                total_ik_tries += 1
+                with planning_scene_monitor.read_only() as scene:
+                    ik_sol = robot_state.set_from_ik("mobile_base_arm", pose_goal, "frankie_hand", 0.5)
+                    if not ik_sol:
+                        continue
+                    robot_state.update()  # required to update transforms
+                    
+                    robot_collision_status = scene.is_state_colliding( 
+                        robot_state=robot_state, joint_model_group_name="mobile_base_arm", verbose=False
+                    )
+                    robot_msg = utils.convert_to_display_robot_state(robotStateToRobotStateMsg(robot_state))
+
+                    robot_state_pub.publish(robot_msg)
+                # print(f"IK {ik_tries} resulted on {robot_collision_status} collision")
+                if not robot_collision_status:
+                    ik_solved = True
+                    print(f"Sampled a valid IK after {ik_tries + 1}")
+                    break
+            if ik_solved: # We try to plan
+                robot.set_start_state(configuration_name="home")
+                robot.set_goal_state(robot_state = robot_state)
+                
+
+                plan = utils.plan(robot, logger)
+                if plan:
+                    working += 1
+                    solved = True
+                    print(f"Problem {exp_name} solved, after ik:{total_ik_tries} and plan tries {plan_tries}")
+                    break
+        
+        data["solved"] = solved
+        data["IK_total"] = total_ik_tries
+        data["IK_avg"] = total_ik_tries/(plan_tries + 1)
+        data["plan_tries"] = plan_tries + 1
+        data["wall_time"] = time.perf_counter() - ti
+
+        print(f"Problem {exp_name} solved {solved}, after ik:{total_ik_tries} and plan tries {plan_tries}")
+        print(data)
+        exp_data.append(data)
+
+        print(f"In total we obtained {working} / {i+1}") 
+    df = pd.DataFrame(exp_data)
+    print(df)
+    df.to_csv("bookshelf_results.csv")
+
+
+if __name__ == "__main__":
+    main()
